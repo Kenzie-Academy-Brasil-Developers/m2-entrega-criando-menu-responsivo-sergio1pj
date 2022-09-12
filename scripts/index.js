abbfa54494122b1d21d8cea9f16814b545d7272c
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-12 11:22:39 -0300

    Feat: img perfil

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,22 @@
 class Menu {
-    static animateLinks() {
-        const li = document.querySelectorAll('.nav__list li');
-        const input = document.querySelector('.input-group input');
-        li.forEach((link) => {
+    static animateLinks(list) {
+        list.forEach((link) => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
                 link.style.animation = "navLinkFade 0.5s ease forwards 0.3s";
             }
         });
-        if (input.style.animation) {
-            input.style.animation = '';
-        }
-        else {
-            input.style.animation = "navLinkFade 0.5s ease forwards 0.3s";
-        }
     }   
     static handleClick() {
         const nav = document.querySelector('.nav__list');
         const inputGroup = document.querySelector('.input-group');
+        const li = document.querySelectorAll('.nav__list li');
+        const childs = document.querySelectorAll('.input-group > *');
         nav.classList.toggle('active');
         inputGroup.classList.toggle('active');
-        Menu.animateLinks();
+        Menu.animateLinks(li);
+        Menu.animateLinks(childs);
     }
     static addClickEvent() {
         const menu = document.querySelector('.menu');
