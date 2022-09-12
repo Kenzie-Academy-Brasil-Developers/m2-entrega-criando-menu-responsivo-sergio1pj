646b439712908cbf5607d5affca267e9d311a14b
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-12 10:47:26 -0300

    feat: input

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 class Menu {
     static animateLinks() {
         const li = document.querySelectorAll('.nav__list li');
+        const input = document.querySelector('.input-group input');
         li.forEach((link) => {
             if (link.style.animation) {
                 link.style.animation = '';
@@ -8,10 +9,18 @@ class Menu {
                 link.style.animation = "navLinkFade 0.5s ease forwards 0.3s";
             }
         });
+        if (input.style.animation) {
+            input.style.animation = '';
+        }
+        else {
+            input.style.animation = "navLinkFade 0.5s ease forwards 0.3s";
+        }
     }   
     static handleClick() {
         const nav = document.querySelector('.nav__list');
+        const inputGroup = document.querySelector('.input-group');
         nav.classList.toggle('active');
+        inputGroup.classList.toggle('active');
         Menu.animateLinks();
     }
     static addClickEvent() {
